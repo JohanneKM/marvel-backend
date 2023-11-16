15fd890d44d1e4928b70f2ad143a828740c720e3
Johanne Khan Malek <johannekhanmalek@Johannes-MacBook-Pro.local>
2023-11-16 16:38:44 +0100

    with search

--- routes/character.js
+++ routes/character.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/characters", async (req, res) => {
+  // pour pouvoir chercher un personnage selon son nom
+  const { name } = req.query;
+  let search = "";
+  const filter = {};
+  if (name) {
+    filter.name = name;
+  }
+
+  for (const key in filter) {
+    search = search + `?${key}=${filter[key]}`;
+    console.log(search);
+  }
   try {
     // console.log(req.query);
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=CMPMRZgEhNVtR0Xv`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=CMPMRZgEhNVtR0Xv${search}`
     );
 
     console.log(response.data);
